6e93bf854cfb108845a1bc7ef56f5a250acb94dc
soila <elizabeth.soila@student.moringaschool.com>
2024-12-07 18:40:41 +0300

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+// Declare variables
+let num1 = 2;
+let num2 = 31;
+let num3 = 28;
+let num4 = 6;
+
+
+let multiply = num1 * num2; 
+
+let random = Math.floor(Math.random() * 80) + 3; 
+
+let mod = num3 % num4; 
+
+let max = Math.max(1, 5, 10, 15, 20); 
+
+console.log (multiply)
+console.log (random)
+console.log (mod)
+console.log (max)
